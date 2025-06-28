6dc53696ac6c12017f95b4b8e26cda0c7ed03f3c
David Chehet <davidchehet28@gmail.com>
2025-06-28 16:54:24 -0400

    Created int add, int sub, int mult, and double divide

--- src/main.c
+++ src/main.c
@@ -1,7 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int subtract(int a, int b) {
+    return a - b;
+}
+
+int multiply(int a, int b) {
+    return a * b;
+}
+
+double divide(int *a, int *b) {
+    double double_a = *a;
+    double double_b = *b;
+    return double_a / double_b;
+}
 
 int main() {
+    int a = 5;
+    int b = 4;
+
     printf("Welcome to the CLI Calculator!\n");
+    printf("Addition: %d\n", add(a, b));
+    printf("Subtraction: %d\n", subtract(a, b));
+    printf("Multiply: %d\n", multiply(a, b));
+    printf("Divide: %.2f\n", divide(&a, &b));
+
     return EXIT_SUCCESS;
 }
